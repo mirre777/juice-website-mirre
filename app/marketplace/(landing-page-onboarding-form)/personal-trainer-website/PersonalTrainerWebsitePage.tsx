b18c82c4e46e33570b47bd4fb710242ae5937eeb
mirre snelting <mirresnelting@gmail.com>
2026-01-07 21:52:54 +0100

    change button text of Ms page to not free

--- app/marketplace/(landing-page-onboarding-form)/personal-trainer-website/PersonalTrainerWebsitePage.tsx
+++ app/marketplace/(landing-page-onboarding-form)/personal-trainer-website/PersonalTrainerWebsitePage.tsx
@@ -587,11 +587,11 @@ export default function PersonalTrainerWebsitePage() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Create for free
+                  Create
                   <ChevronRight className="w-5 h-5 ml-2" />
                 </a>
               </Button>
-              <p className="text-sm text-gray-800 mt-3 max-w-md">We won't charge you for creating the page.</p>
+              <p className="text-sm text-gray-800 mt-3 max-w-md">We won't charge you for creating the page, only for publishing it.</p>
             </div>
           </div>
         </div>
